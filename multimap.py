6ebb2467c5ac6d9c7e59747c2eff2f0d49abd3cd
Mike Boers <github@mikeboers.com>
2009-10-01 00:45:08 -0400

    - Created read-only and mutable variations of multimaps.

--- multimap.py
+++ multimap.py
@@ -39,55 +39,46 @@ class MultiMap(collections.Mapping):
     >>> m
     MultiMap([('a', 1), ('b', 2)])
     
+    >>> m = MultiMap([('a', 1), ('b', 2), ('c', 3), ('c', 4)])
+    >>> m
+    MultiMap([('a', 1), ('b', 2), ('c', 3), ('c', 4)])
+    
     
     >>> m['a']
     1
     
-    >>> m['c'] = 3
-    >>> m['c']
-    3
-    
-    >>> m.setlist('c', [1, 2, 3])
-    >>> m['c']
-    1
     >>> m.getall('c')
-    [1, 2, 3]
+    [3, 4]
     
     >>> m.keys()
     ['a', 'b', 'c']
     >>> m.allkeys()
-    ['a', 'b', 'c', 'c', 'c']
+    ['a', 'b', 'c', 'c']
     >>> m.allvalues()
-    [1, 2, 1, 2, 3]
+    [1, 2, 3, 4]
     >>> len(m)
     3
     >>> m.alllen()
-    5
+    4
     
-    >>> m['c'] = 4
-    >>> m.getall('c')
-    [4]
-    
-    >>> m.append((1, 2))
-    >>> m.allitems()
-    [('a', 1), ('b', 2), ('c', 4), (1, 2)]
-    
-    >>> m.pop()
-    (1, 2)
-    
-    >>> m.pop(0)
-    ('a', 1)
+    >>> m['d'] = 5
+    Traceback (most recent call last):
+    ...
+    TypeError: 'MultiMap' object does not support item assignment
     
     """
     
     def __init__(self, *args, **kwargs):
-        self._pairs = []
+        self.__pairs = []
         for arg in args:
             if isinstance(arg, collections.Mapping):
-                self.update(arg)
+                for x in arg.items():
+                    self.__pairs.append(self._conform_pair(x))
             else:
-                self.extend(arg)
-        self.update(kwargs)
+                for x in arg:
+                    self.__pairs.append(self._conform_pair(x))
+        for x in kwargs.items():
+            self.__pairs.append(self._conform_pair(x))
     
     def _conform_key(self, key):
         return key
@@ -95,32 +86,38 @@ class MultiMap(collections.Mapping):
     def _conform_value(self, value):
         return value
     
+    def _conform_pair(self, pair):
+        pair = tuple(pair)
+        if len(pair) != 2:
+            raise ValueError('pair must be length 2')
+        return (self._conform_key(pair[0]), self._conform_value(pair[1]))
+        
     def __repr__(self):
-        return '%s(%r)' % (self.__class__.__name__, self._pairs)
+        return '%s(%r)' % (self.__class__.__name__, self.__pairs)
     
     def __nonzero__(self):
-        return len(self._pairs)
+        return len(self.__pairs)
     
     def __getitem__(self, key):
         key = self._conform_key(key)
-        for x in self._pairs:
+        for x in self.__pairs:
             if x[0] == key:
                 return x[1]
         raise KeyError(key)
     
     def __len__(self):
-        return len(set(x[0] for x in self._pairs))
+        return len(set(x[0] for x in self.__pairs))
     
     def alllen(self):
-        return len(self._pairs)
+        return len(self.__pairs)
     
     def getall(self, key):
         key = self._conform_key(key)
-        return [x[1] for x in self._pairs if x[0] == key]
+        return [x[1] for x in self.__pairs if x[0] == key]
     
     def iteritems(self):
         keys_yielded = set()
-        for k, v in self._pairs:
+        for k, v in self.__pairs:
             if k not in keys_yielded:
                 keys_yielded.add(k)
                 yield k, v
@@ -135,11 +132,11 @@ class MultiMap(collections.Mapping):
         return iter(self)
     
     def iterallkeys(self):
-        for x in self._pairs:
+        for x in self.__pairs:
             yield x[0]
     
     def allkeys(self):
-        return [x[0] for x in self._pairs]
+        return [x[0] for x in self.__pairs]
     
     def items(self):
         return list(self.iteritems())
@@ -151,22 +148,88 @@ class MultiMap(collections.Mapping):
         return list(self.itervalues())
     
     def iterallvalues(self):
-        for x in self._pairs:
+        for x in self.__pairs:
             return x[1]
     
     def allvalues(self):
-        return [x[1] for x in self._pairs]
+        return [x[1] for x in self.__pairs]
     
     def iterallitems(self):
-        return iter(self._pairs)
+        return iter(self.__pairs)
     
     def allitems(self):
-        return self._pairs[:]
+        return self.__pairs[:]
+
+
+class MutableMultiMap(MultiMap, collections.MutableMapping):
+    """An ordered mapping which supports multiple values for the same key.
+
+    >>> m = MutableMultiMap({'a': 1, 'b': 2})
+    >>> m
+    MutableMultiMap([('a', 1), ('b', 2)])
+
+    >>> m = MutableMultiMap([('a', 1), ('b', 2)])
+    >>> m
+    MutableMultiMap([('a', 1), ('b', 2)])
+
+    >>> m = MutableMultiMap(a=1, b=2)
+    >>> m
+    MutableMultiMap([('a', 1), ('b', 2)])
+
+
+    >>> m['a']
+    1
+
+    >>> m['c'] = 3
+    >>> m['c']
+    3
+
+    >>> m.setlist('c', [1, 2, 3])
+    >>> m['c']
+    1
+    >>> m.getall('c')
+    [1, 2, 3]
+
+    >>> m.keys()
+    ['a', 'b', 'c']
+    >>> m.allkeys()
+    ['a', 'b', 'c', 'c', 'c']
+    >>> m.allvalues()
+    [1, 2, 1, 2, 3]
+    >>> len(m)
+    3
+    >>> m.alllen()
+    5
+
+    >>> m['c'] = 4
+    >>> m.getall('c')
+    [4]
+
+    >>> m.append((1, 2))
+    >>> m.allitems()
+    [('a', 1), ('b', 2), ('c', 4), (1, 2)]
+
+    >>> m.pop()
+    (1, 2)
+
+    >>> m.pop(0)
+    ('a', 1)
+
+    """
+    
+    @property
+    def _pairs(self):
+        # This is such a hack!
+        return self._MultiMap__pairs
+    
+    @_pairs.setter
+    def _pairs(self, value):
+        self._MultiMap__pairs = value
     
     def __delitem__(self, key):
         key = self._conform_key(key)
         self._pairs = [x for x in self._pairs if x[0] != key]
-    
+
     def __setitem__(self, key, value):
         key = self._conform_key(key)
         if isinstance(value, (tuple, list)):
@@ -174,43 +237,36 @@ class MultiMap(collections.Mapping):
         else:
             del self[key]
             self._pairs.append((self._conform_key(key), self._conform_value(value)))
-    
+
     def setlist(self, key, value):
         key = self._conform_key(key)
         del self[key]
         for v in value:
             self._pairs.append((key, self._conform_value(v)))
-    
+
     def sort(self, *args, **kwargs):
         self._pairs.sort(*args, **kwargs)
-    
-    def _conform_pair(self, pair):
-        pair = tuple(pair)
-        if len(pair) != 2:
-            raise ValueError('pair must be length 2')
-        return (self._conform_key(pair[0]), self._conform_value(pair[1]))
-            
+
     def append(self, pair):
         self._pairs.append(self._conform_pair(pair))
-    
+
     def extend(self, pairs):
         self._pairs.extend(self._conform_pair(x) for x in pairs)
-    
+
     def pop(self, *args):
         return self._pairs.pop(*args)
-    
+
     def insert(self, index, pair):
         self._pairs.insert(index, self._conform_pair(pair))
-    
+
     def update(self, mapping):
         for k, v in mapping.items():
             del self[k]
             self[k] = v
-    
+
     def copy(self):
         return self.__class__(self._pairs[:])
     
-
 if __name__ == '__main__':
     from . import test
     test.run()
