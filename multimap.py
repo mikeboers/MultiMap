223bc0f1e28918b79435044051e9275607a1a560
Mike Boers <github@mikeboers.com>
2009-10-05 15:31:23 -0400

    - Floats in signature (times) are represented as base64 encoded packed binaries.

--- multimap.py
+++ multimap.py
@@ -20,12 +20,15 @@ with no word seperators. Oh well.
 """
 
 # Setup path for local evaluation.
-# When copying to another file, just change the __package__ to be accurate.
+# When copying to another file, just change the parameter to be accurate.
 if __name__ == '__main__':
-    import sys
-    __package__ = 'nitrogen'
-    sys.path.insert(0, __file__[:__file__.rfind('/' + __package__.split('.')[0])])
-    __import__(__package__)
+    def __local_eval_fix(package):
+        global __package__
+        import sys
+        __package__ = package
+        sys.path.insert(0, '/'.join(['..'] * (1 + package.count('.'))))
+        __import__(__package__)
+    __local_eval_fix('nitrogen')
 
 import collections
 
