ca965d1b4abff235c0da06e2f6ddc11305e9e1cc
Mike Boers <github@mikeboers.com>
2009-12-06 14:21:47 -0500

    removed local eval setup

--- multimap.py
+++ multimap.py
@@ -19,16 +19,6 @@ with no word seperators. Oh well.
 
 """
 
-# Setup path for local evaluation.
-# When copying to another file, just change the parameter to be accurate.
-if __name__ == '__main__':
-    def __local_eval_fix(package):
-        global __package__
-        import sys
-        __package__ = package
-        sys.path.insert(0, '/'.join(['..'] * (1 + package.count('.'))))
-        __import__(__package__)
-    __local_eval_fix('nitrogen')
 
 import collections
 
