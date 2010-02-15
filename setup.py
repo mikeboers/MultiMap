39b7807dfd59d9f00778be45b4fd3d86eeb2abe4
Mike Boers <github@mikeboers.com>
2010-02-14 23:08:20 -0500

    Getting ready for intial release.

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+    name='multimap',
+    version='1.0b',
+    description='Mapping class which allows multiple values per key and preserves order.',
+    url='http://github.com/mikeboers/multimap',
+    py_modules=['multimap'],
+    
+    author='Mike Boers',
+    author_email='nitrogen@mikeboers.com',
+    license='New BSD License',
+)
