a4c84dfb246f746480f170678585f25b612178c6
Mike Boers <github@mikeboers.com>
2011-04-05 10:12:00 -0400

    Add classifiers.

--- setup.py
+++ setup.py
@@ -9,6 +9,16 @@ setup(
     py_modules=['multimap'],
     
     author='Mike Boers',
-    author_email='nitrogen@mikeboers.com',
-    license='New BSD License',
+    author_email='multimap@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
