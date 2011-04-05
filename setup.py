0251e5d5df693cc247b4ac5b95adfdd10e3bec04
Mike Boers <github@mikeboers.com>
2011-04-05 10:12:35 -0400

    Bump version number for classifiers.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='MultiMap',
-    version='1.0.2',
+    version='1.0.3',
     description='Mapping class which allows multiple values per key and preserves order by value; values with the same key are not grouped together.',
     url='http://github.com/mikeboers/multimap',
     py_modules=['multimap'],
